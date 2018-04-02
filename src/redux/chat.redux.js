f6d4588f48c1aa360df56efb63aff199486dacc8
justin5202 <386659643@qq.com>
2018-04-02 23:18:47 +0800

    修正未读消息数量

--- src/redux/chat.redux.js
+++ src/redux/chat.redux.js
@@ -15,38 +15,39 @@ const initState = {
 	unread: 0
 }
 
-export function chat(state=initState, action) {
-	switch(action.type) {
+export function chat(state = initState, action) {
+	switch (action.type) {
 		case MSG_LIST:
-			return {...state, users: action.payload.users, chatmsg: action.payload.msgs, unread: action.payload.msgs.filter(v=>!v.read && v.to === action.payload.userid).length}
+			return { ...state, users: action.payload.users, chatmsg: action.payload.msgs, unread: action.payload.msgs.filter(v => !v.read && v.to === action.payload.userid).length }
 		case MSG_RECV:
 			const n = action.payload.msg.to === action.payload.userid ? 1 : 0
-			return {...state, chatmsg: [...state.chatmsg, action.payload.msg], unread: state.unread + n}
+			return { ...state, chatmsg: [...state.chatmsg, action.payload.msg], unread: state.unread + n }
 		case MSG_READ:
-			return {...state, chatmsg: state.chatmsg.map(v => ({...v, read: true})), unread: state.unread - action.payload.num}
+			const { from, num } = action.payload
+			return { ...state, chatmsg: state.chatmsg.map(v => ({ ...v, read: from === v.from ? true : v.read })), unread: state.unread - num }
 		default:
 			return state
 	}
 }
 
 function msgList(msgs, users, userid) {
-	return {type: 'MSG_LIST', payload: {msgs, users, userid}}
+	return { type: 'MSG_LIST', payload: { msgs, users, userid } }
 }
 
 function msgRecv(msg, userid) {
-	return {type: 'MSG_RECV', payload: {msg, userid}}
+	return { type: 'MSG_RECV', payload: { msg, userid } }
 }
 
-function msgRead({from, userid, num}) {
-	return {type: MSG_READ, payload: {from, userid, num}}
+function msgRead({ from, userid, num }) {
+	return { type: MSG_READ, payload: { from, userid, num } }
 }
 
 export function readMsg(from) {
 	return (dispatch, getState) => {
-		axios.post('/user/readmsg', {from})
+		axios.post('/user/readmsg', { from })
 			.then(res => {
 				const userid = getState().user._id
-				if(res.status === 200 && res.data.code === 0) {
+				if (res.status === 200 && res.data.code === 0) {
 					dispatch(msgRead(userid, from, res.data.num))
 				}
 			})
@@ -54,7 +55,7 @@ export function readMsg(from) {
 }
 
 export function recvMsg() {
-	return (dispatch, getState)=> {
+	return (dispatch, getState) => {
 		socket.on('recvmsg', (data) => {
 			// 当前用户id
 			const userid = getState().user._id
@@ -63,17 +64,17 @@ export function recvMsg() {
 	}
 }
 
-export function sendMsg({from, to, msg}) {
-	return dispatch=> {
-		socket.emit('sendmsg', {from, to, msg})
+export function sendMsg({ from, to, msg }) {
+	return dispatch => {
+		socket.emit('sendmsg', { from, to, msg })
 	}
 }
 
 export function getMsgList() {
-	return (dispatch, getState)=> {
+	return (dispatch, getState) => {
 		axios.get('/user/getmsglist')
 			.then(res => {
-				if(res.status === 200 && res.data.code === 0) {
+				if (res.status === 200 && res.data.code === 0) {
 					// 当前用户id
 					const userid = getState().user._id
 					dispatch(msgList(res.data.msgs, res.data.users, userid))
